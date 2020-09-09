478f3630ca512724026309f37a06fda91687075b
Murilo Fernandes Dellize <murilo46dellize@gmail.com>
2020-09-08 23:10:13 -0300

    Aula EF Core Concluído

---
+++ EF_Core/Domains/Pedido.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace EF_Core.Domains
+{
+    public class Pedido
+    {
+        [Key]
+        public  Guid Id { get; set; }
+        public string Status { get; set; }
+        public DateTime OrderDate { get; set; }
+
+        public Pedido()
+        {
+            Id = Guid.NewGuid();
+        }
+
+    }
+}
---
+++ EF_Core/Domains/Produto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EF_Core.Domains
+{
+
+    //Define a classe do produto
+    public class Produto
+    {
+
+        [Key]
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public float Preco { get; set; }
+
+        public Produto()
+        {
+            Id = Guid.NewGuid();
+        }
+
+    }
+
+}
