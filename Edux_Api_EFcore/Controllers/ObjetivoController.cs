a515cedea35cb1041f9c62d9cba51c76f692a4ab
Murilo Fernandes Dellize <murilo46dellize@gmail.com>
2020-09-25 01:33:25 -0300

    Domains e Context Concluídos

---
+++ Edux_Api_EFcore/Controllers/ObjetivoController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Edux_Api_EFcore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ObjetivoController : ControllerBase
+    {
+        // GET: api/<ObjetivoController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<ObjetivoController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ObjetivoController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ObjetivoController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ObjetivoController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
